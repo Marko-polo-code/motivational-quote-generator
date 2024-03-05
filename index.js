18a97edbf1ec7edb2baf048b16d71a743fdc476c
Mark Muller <mark.muller.w@gmail.com>
2024-03-05 18:16:47 +0100

    added 3 parts to create multiple combinations possible

---
+++ index.js
@@ -0,0 +1,24 @@
+// Define arrays containing quotes for each part of the message
+const part1 = [
+  "Success is not final",
+  "The only way to do great work",
+  "The greatest glory",
+  "Believe you can",
+  "Opportunities don't happen"
+];
+
+const part2 = [
+  "is the courage",
+  "is to love what you do",
+  "in living",
+  "and you're halfway there",
+  "you create them"
+];
+
+const part3 = [
+  "to continue",
+  "and keep going",
+  "a life well-lived",
+  "of success",
+  "make the most of it"
+];
